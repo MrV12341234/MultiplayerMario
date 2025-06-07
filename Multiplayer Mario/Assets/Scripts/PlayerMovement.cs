fbcb22f47b969a35935ce933cd644fffed099890
MrV12341234 <Scottvlee125@gmail.com>
2025-06-07 23:35:33 +0800

    Movement updates

--- Multiplayer Mario/Assets/Scripts/PlayerMovement.cs
+++ Multiplayer Mario/Assets/Scripts/PlayerMovement.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class NewMonoBehaviourScript : MonoBehaviour
@@ -7,10 +9,17 @@ public class NewMonoBehaviourScript : MonoBehaviour
 
     private Vector2 velocity;
     private float inputAxis;
-    
-    public float moveSpeed = 8f;
 
-    private void Awake()
+    public float moveSpeed = 8f;
+    public float maxJumpHeight = 5f;
+    public float maxJumpTime = 1f;
+    public float jumpForce => (2f * maxJumpHeight) / (maxJumpTime / 2f);
+    public float gravity => (-2f * maxJumpHeight) / Mathf.Pow((maxJumpTime / 2f), 2);
+
+    public bool grounded { get; private set; }
+    public bool jumping { get; private set; }
+
+private void Awake()
     {
         rigidbody = GetComponent<Rigidbody2D>();
         camera = Camera.main;
@@ -19,12 +28,55 @@ public class NewMonoBehaviourScript : MonoBehaviour
     private void Update()
     {
         HorizontalMovement();
+        // checks if you're grounded
+        grounded = rigidbody.Raycast(Vector2.down);
+        if (grounded)
+        {
+            GroundedMovement();
+        }
+
+        ApplyGravity();
     }
 
     private void HorizontalMovement()
     {
         inputAxis = Input.GetAxis("Horizontal");
         velocity.x = Mathf.MoveTowards(velocity.x, inputAxis * moveSpeed, moveSpeed * Time.deltaTime);
+
+        if (rigidbody.Raycast(Vector2.right * velocity.x))
+        {
+            velocity.x = 0f;
+        }
+        
+        if (velocity.x > 0f)
+        {
+            transform.eulerAngles = Vector2.zero;
+        } 
+        else if (velocity.x < 0f)
+        {
+            transform.eulerAngles = new Vector3(0f, 180f, 0f);
+        }
+    }
+
+    private void GroundedMovement()
+    {
+        velocity.y = Mathf.Max(velocity.y, 0f);
+        jumping = velocity.y > 0f;
+        if (Input.GetButtonDown("Jump"))
+        {
+            velocity.y = jumpForce;
+            jumping = true;
+        }
+    }
+
+    private void ApplyGravity()
+    {
+        //below physics allow the game to feel like the real mario where holding jump button gives you less gravity but when you release you start falling.
+        bool falling = velocity.y < 0f || !Input.GetButton("Jump");
+        float multiplier = falling ? 2f : 1f;
+        
+        velocity.y += gravity * multiplier * Time.deltaTime;
+        velocity.y = Mathf.Max(velocity.y, gravity / 2f);
     }
 
     private void FixedUpdate()
@@ -40,5 +92,15 @@ public class NewMonoBehaviourScript : MonoBehaviour
         rigidbody.MovePosition(position);
         
     }
-    
+    // OnCollisionEnter2D is a unity function that says 'do this when player has collided with something'
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.layer != LayerMask.NameToLayer("PowerUp"))
+        {
+            // Stop vertical movement if mario bonks his head
+            if (transform.DotTest(collision.transform, Vector2.up)) {
+                 velocity.y = 0f;
+             }
+        }
+    }
 }
