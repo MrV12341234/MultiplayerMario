a1c2182b25c7dc900f4004fc9b88aa1698a47ef2
MrV12341234 <Scottvlee125@gmail.com>
2025-06-15 14:38:39 +0800

    Koopa shelled and death barrier

--- Multiplayer Mario/Assets/Scripts/Goomba.cs
+++ Multiplayer Mario/Assets/Scripts/Goomba.cs
@@ -22,6 +22,14 @@ public class Goomba : MonoBehaviour
         }
     }
 
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer("Shell"))
+        {
+            Hit();
+        }
+    }
+
     private void Flatten() // function when enemy is killed
     {
         GetComponent<Collider2D>().enabled = false;
@@ -30,5 +38,11 @@ public class Goomba : MonoBehaviour
         GetComponent<SpriteRenderer>().sprite = flatSprite;
         Destroy(gameObject, 0.5f); // wait 5 seconds before destroying so player has .5 seconds to see flattened goomba
     }
-    
+
+    private void Hit()
+    {
+        GetComponent<AnimatedSprite>().enabled = false;
+        GetComponent<DeathAnimation>().enabled = true;
+        Destroy(gameObject, 3f);
+    }
 }
--- Multiplayer Mario/Assets/Scripts/Koopa.cs
+++ Multiplayer Mario/Assets/Scripts/Koopa.cs
@@ -3,13 +3,17 @@ using UnityEngine;
 public class Koopa : MonoBehaviour
 {
     public Sprite shellSprite;
+    public float shellSpeed = 12f;
+
+    private bool shelled;
+    private bool pushed;
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        // check what Goomba collided with
-        if (collision.gameObject.CompareTag("Player"))
+        // check what Koopa collided with
+        if (!shelled && collision.gameObject.CompareTag("Player") && collision.gameObject.TryGetComponent(out Player player)) // confirms its the player that collides with koopa
         {
-            Player player = collision.gameObject.GetComponent<Player>();
+            
             // dot task to ensure kill only when mario hits goomba going down
             if (collision.transform.DotTest(transform, Vector2.down))
             {
@@ -21,12 +25,58 @@ public class Koopa : MonoBehaviour
             }
         }
     }
+    
 
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (shelled && other.CompareTag("Player")) // confirms its the player that collides with koopa in a shell
+        {
+            if (!pushed)
+            {
+                Vector2 direction = new Vector2(transform.position.x - other.transform.position.x, 0f);
+                PushShell(direction);
+            }
+            else
+            {
+                Player player = other.GetComponent<Player>(); // gets a reference to the player
+                player.Hit();
+            }
+        }
+        else if (!shelled && other.gameObject.layer == LayerMask.NameToLayer("Shell"))
+        {
+            Hit();
+        }
+    }
+
+    
     private void EnterShell() // function when enemy is killed
     {
+        shelled = true;
+        
         GetComponent<EntityMovement>().enabled = false;
         GetComponent<AnimatedSprite>().enabled = false;
         GetComponent<SpriteRenderer>().sprite = shellSprite;
+    }
+
+    private void PushShell(Vector2 direction)
+    {
+        pushed = true;
+
+        GetComponent<Rigidbody2D>().isKinematic = false;
         
+        EntityMovement movement = GetComponent<EntityMovement>();
+        movement.direction = direction.normalized;
+        movement.speed = shellSpeed;
+        movement.enabled = true;
+
+        gameObject.layer = LayerMask.NameToLayer("Shell");
+        
+    }
+
+    private void Hit()
+    {
+        GetComponent<AnimatedSprite>().enabled = false;
+        GetComponent<DeathAnimation>().enabled = true;
+        Destroy(gameObject, 3f);
     }
 }
